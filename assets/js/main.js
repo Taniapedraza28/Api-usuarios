af05a49b24ac2cf828fbe4f198b4ea4b2d62db25
tany <tany_a210@hotmail.com>
2022-10-04 21:32:05 -0500

    cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,35 @@
 console.log("Entro al main.js");
+const base_url_api = "http://ucamp.alumnos.dev4humans.com.mx";
+const tblUsuarios = document.getElementById("tblUsuarios");
+
+function cargarUsuarios() {
+    fetch(base_url_api + "/Main/alumnos",
+        { method: "GET" })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            tblUsuarios.innerHTML = "";
+            for (const usuario of result.data) {
+                let tr = `<tr>
+                <td>${usuario.id}</td>
+                <td>${usuario.nombre}</td>
+                <td>${usuario.paterno}</td>
+                <td>${usuario.materno}</td>
+                <td>${usuario.nombre}</td>
+                </tr>`;
+                tblUsuarios.innerHTML += tr;
+            }
+
+        })
+        .catch(error => {
+            console.log("Error detectado!");
+        })
+}
+
+
+function agregarUsuario() {
+    console.log("Entro a agregarUsuario");
+}
+
+
+cargarUsuarios();
